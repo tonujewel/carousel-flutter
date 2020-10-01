84d68689f529f95fe7b939841e357f3766f43e34
Jewel Rana <tonujewel@gmail.com>
2020-10-01 12:10:41 +0600

    Done

---
+++ android/app/src/main/kotlin/com/lastorbital/flutter_carosel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.lastorbital.flutter_carosel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:carousel_pro/carousel_pro.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_carosel/components/CarouselSlider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.brown,
+        title: Text("Carousel example"),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Padding(
+              padding: EdgeInsets.only(left: 20, right: 20, top: 10),
+              child: CarouselSlider()
+          )
+        ],
+      ),
+    );
+  }
+}
